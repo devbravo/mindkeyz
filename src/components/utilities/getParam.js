8f7a6b85829a96eb6ac61d26befa6f2346327b35
Diego <diegofranco711@gmail.com>
2021-04-11 20:32:51 -0300

    Added jsdocs to getParam function

--- src/components/utilities/getParam.js
+++ src/components/utilities/getParam.js
@@ -1,5 +1,11 @@
 import { useParams } from 'react-router-dom';
 
+/**
+ * function using useParams hook, to get the parameter.
+ * @function GetParam
+ * @returns {boolean}
+ */
+
 function GetParam() {
   const { id } = useParams();
 
